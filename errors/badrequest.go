a15e0a367fed614808af81d97dad06b2fab00413
Alejandro Mery <amery@jpi.io>
2021-04-22 12:41:32 +0000

    errors: introduce BadRequestError{}
    Signed-off-by: Alejandro Mery <amery@jpi.io>

---
+++ errors/badrequest.go
@@ -0,0 +1,72 @@
+package errors
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type BadRequestError struct {
+	Errors []error
+}
+
+func (err *BadRequestError) AppendError(e error) {
+	err.Errors = append(err.Errors, e)
+}
+
+func (err *BadRequestError) AppendErrorString(s string) {
+	err.AppendError(errors.New(s))
+}
+
+func (err *BadRequestError) AppendErrorf(s string, args ...interface{}) {
+	var e error
+
+	if len(args) > 0 {
+		e = fmt.Errorf(s, args...)
+	} else {
+		e = errors.New(s)
+	}
+
+	err.AppendError(e)
+}
+
+func (err *BadRequestError) Ok() bool {
+	return len(err.Errors) == 0
+}
+
+func (err *BadRequestError) Status() int {
+	if len(err.Errors) == 0 {
+		return http.StatusOK
+	} else {
+		return http.StatusBadRequest
+	}
+}
+
+func (err *BadRequestError) String() string {
+	var errors []string
+	for _, e := range err.Errors {
+		errors = append(errors, e.Error())
+	}
+	return strings.Join(errors, "\n")
+}
+
+func (err *BadRequestError) Error() string {
+	return ErrorText(err.Status())
+}
+
+func (err *BadRequestError) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	code := err.Status()
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.WriteHeader(code)
+
+	fmt.Fprintln(w, ErrorText(code))
+	if code != http.StatusOK {
+		fmt.Fprintln(w)
+		for _, e := range err.Errors {
+			fmt.Fprintln(w, e.Error())
+		}
+	}
+}
