4eda7603408fc85dd814269c0eeb5f9abc629bd8
Alejandro Mery <amery@jpi.io>
2021-04-30 22:14:10 +0000

    types: avoid conflicting standards by changing Handler to use TryServeHTTP()
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- types.go
+++ types.go
@@ -14,7 +14,7 @@ type Error interface {
 type HandlerFunc func(http.ResponseWriter, *http.Request) error
 
 type Handler interface {
-	Handle(http.ResponseWriter, *http.Request) error
+	TryServeHTTP(http.ResponseWriter, *http.Request) error
 }
 
 type MiddlewareHandlerFunc func(http.Handler) http.Handler
