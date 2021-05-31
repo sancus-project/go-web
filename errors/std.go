157d5c8b3ed44a6f7b26cbafd4e8a1bc17f1a481
Alejandro Mery <amery@jpi.io>
2021-05-31 15:20:38 +0100

    errors: borrow Is/As/Unwrap from the standard "errors" package
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- errors/std.go
+++ errors/std.go
@@ -12,3 +12,15 @@ func New(s string, args ...interface{}) error {
 
 	return errors.New(s)
 }
+
+func Is(err, target error) bool {
+	return errors.Is(err, target)
+}
+
+func As(err error, target interface{}) bool {
+	return errors.As(err, target)
+}
+
+func Unwrap(err error) error {
+	return errors.Unwrap(err)
+}
