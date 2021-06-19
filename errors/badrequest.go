e34d9d0d005513c3e12ebf8de49abdd0e3485f94
Alejandro Mery <amery@jpi.io>
2021-06-19 19:57:21 +0100

    errors: refactor BadRequestError using go.sancus.dev/core/errors.ErrorStack
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- errors/badrequest.go
+++ errors/badrequest.go
@@ -3,47 +3,20 @@ package errors
 import (
 	"fmt"
 	"net/http"
-	"strings"
+
+	"go.sancus.dev/core/errors"
 )
 
 type BadRequestError struct {
-	Errors []error
-}
-
-func (err *BadRequestError) AppendError(e error) {
-	err.Errors = append(err.Errors, e)
-}
-
-func (err *BadRequestError) AppendErrorString(s string) {
-	err.AppendError(New(s))
-}
-
-func (err *BadRequestError) AppendErrorf(s string, args ...interface{}) {
-	err.AppendError(New(s, args...))
-}
-
-func (err *BadRequestError) Ok() bool {
-	return len(err.Errors) == 0
+	errors.ErrorStack
 }
 
 func (err *BadRequestError) Status() int {
-	if len(err.Errors) == 0 {
+	if err.Ok() {
 		return http.StatusOK
 	} else {
 		return http.StatusBadRequest
 	}
-}
-
-func (err *BadRequestError) String() string {
-	var errors []string
-	for _, e := range err.Errors {
-		errors = append(errors, e.Error())
-	}
-	return strings.Join(errors, "\n")
-}
-
-func (err *BadRequestError) Error() string {
-	return ErrorText(err.Status())
 }
 
 func (err *BadRequestError) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -56,7 +29,7 @@ func (err *BadRequestError) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, ErrorText(code))
 	if code != http.StatusOK {
 		fmt.Fprintln(w)
-		for _, e := range err.Errors {
+		for _, e := range err.Errors() {
 			fmt.Fprintln(w, e.Error())
 		}
 	}
