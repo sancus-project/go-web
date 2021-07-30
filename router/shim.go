a632878d0a63213e1e81df67298bccd5cc1a62cc
Alejandro Mery <amery@jpi.io>
2021-07-30 12:31:36 +0100

    router: add type router trampoline
    Signed-off-by: Alejandro Mery <amery@jpi.io>

---
+++ router/shim.go
@@ -0,0 +1,96 @@
+package router
+
+import (
+	"net/http"
+
+	"go.sancus.dev/core/errors"
+	"go.sancus.dev/web"
+)
+
+// a router is a trampoline for most Router methods
+type router struct {
+	getNode func(path string) *node
+}
+
+func (r *router) Handle(path string, h http.Handler) {
+	r.getNode(path).handle(h)
+}
+
+func (r *router) HandleFunc(path string, h http.HandlerFunc) {
+	r.getNode(path).handle(h)
+}
+
+func (r *router) TryHandle(path string, h web.Handler) {
+	r.getNode(path).tryHandle(h)
+}
+
+func (r *router) TryHandleFunc(path string, h web.HandlerFunc) {
+	r.getNode(path).tryHandle(h)
+}
+
+func (r *router) Method(method string, path string, h http.Handler) {
+	r.getNode(path).method(method, h)
+}
+
+func (r *router) MethodFunc(method string, path string, h http.HandlerFunc) {
+	r.getNode(path).method(method, h)
+}
+
+func (r *router) TryMethod(method string, path string, h web.Handler) {
+	r.getNode(path).tryMethod(method, h)
+}
+
+func (r *router) TryMethodFunc(method string, path string, h web.HandlerFunc) {
+	r.getNode(path).tryMethod(method, h)
+}
+
+// just a node on the trie but allowing it to
+// migrate between raw and ready states
+type node struct {
+	Handler
+}
+
+func (n *node) toolate(fn string) {
+	err := errors.New("Can't call %s() anymore", fn)
+	panic(errors.WithStackTrace(2, err))
+}
+
+func (n *node) handle(h http.Handler) {
+	if v, ok := n.Handler.(interface {
+		handle(http.Handler)
+	}); ok {
+		v.handle(h)
+	} else {
+		n.toolate("Handle")
+	}
+}
+
+func (n *node) tryHandle(h web.Handler) {
+	if v, ok := n.Handler.(interface {
+		tryHandle(web.Handler)
+	}); ok {
+		v.tryHandle(h)
+	} else {
+		n.toolate("TryHandle")
+	}
+}
+
+func (n *node) method(method string, h http.Handler) {
+	if v, ok := n.Handler.(interface {
+		method(string, http.Handler)
+	}); ok {
+		v.method(method, h)
+	} else {
+		n.toolate("Method")
+	}
+}
+
+func (n *node) tryMethod(method string, h web.Handler) {
+	if v, ok := n.Handler.(interface {
+		tryMethod(string, web.Handler)
+	}); ok {
+		v.tryMethod(method, h)
+	} else {
+		n.toolate("TryMethod")
+	}
+}
