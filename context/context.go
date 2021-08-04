db3cfff7534551ff93df53918a874103c6d8d0ba
Alejandro Mery <amery@jpi.io>
2021-08-04 16:09:57 +0100

    context: split RoutePath from context.Context related things
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- context/context.go
+++ context/context.go
@@ -1,12 +1,7 @@
 package context
 
 import (
-	"log"
-	"path/filepath"
-	"strings"
-
 	"go.sancus.dev/core/context"
-	"go.sancus.dev/core/errors"
 )
 
 type Context struct {
@@ -18,116 +13,6 @@ type Context struct {
 // Clone() creates a copy of a routing Context object
 func (rctx Context) Clone() *Context {
 	return &rctx
-}
-
-func (rctx *Context) Init(prefix, path string) {
-	var pattern string
-
-	if prefix == "" {
-		prefix = "/"
-	}
-
-	if path == "" {
-		pattern = prefix
-	} else if n := strings.IndexRune(path[1:], '/'); n < 0 {
-		pattern = filepath.Join(prefix, path)
-	} else {
-		pattern = filepath.Join(prefix, "*")
-	}
-
-	*rctx = Context{
-		RoutePrefix:  prefix,
-		RoutePattern: pattern,
-		RoutePath:    path,
-	}
-}
-
-func (rctx *Context) Path() string {
-	var s string
-
-	prefix := rctx.RoutePrefix
-	path := rctx.RoutePath
-
-	if prefix == "/" {
-		s = path
-	} else if path == "" {
-		s = prefix
-	} else {
-		s = prefix + path
-	}
-
-	return s
-}
-
-func (rctx *Context) Step(prefix string) *Context {
-	var path string
-
-	pattern := strings.TrimSuffix("/*", rctx.RoutePattern)
-
-	if prefix == rctx.RoutePath {
-		// prefix is the whole RoutePath
-		pattern += prefix
-		prefix = rctx.Path()
-	} else if s := strings.TrimPrefix(rctx.RoutePath, prefix); s != rctx.RoutePath {
-		// prefix is part of the RoutePath
-		pattern += prefix + "/*"
-		if rctx.RoutePrefix != "/" {
-			prefix = rctx.RoutePrefix + prefix
-		}
-
-		path = s
-	} else if n := strings.Index(rctx.RoutePath, prefix); n < 0 {
-		err := errors.New("%+n: prefix:%q incompatible (%#v)",
-			errors.Here(), prefix, rctx)
-		log.Fatal(err)
-	} else {
-		// offset... resuming nested routing
-		l := n + len(prefix)
-
-		prefix = rctx.RoutePath[:l]
-		path = rctx.RoutePath[l:]
-
-		if prefix+s != rctx.RoutePath {
-			err := errors.New("%+n: BUG: prefix:%q + path:%q != %q",
-				errors.Here(), prefix, path, rctx.RoutePath)
-			log.Fatal(err)
-		}
-
-		pattern += prefix + "/*"
-		if rctx.RoutePrefix != "/" {
-			prefix = rctx.RoutePrefix + prefix
-		}
-	}
-
-	next := &Context{
-		RoutePath:    path,
-		RoutePrefix:  prefix,
-		RoutePattern: pattern,
-	}
-
-	return next
-}
-
-func (rctx *Context) Next() (*Context, string) {
-
-	path := rctx.RoutePath
-	if len(path) > 1 {
-		var prefix string
-
-		s := path[1:]
-		if n := strings.IndexRune(s, '/'); n < 0 {
-			prefix = path
-		} else {
-			s = s[:n]
-			prefix = path[:n+1]
-		}
-
-		next := rctx.Step(prefix)
-
-		return next, s
-	}
-
-	return nil, ""
 }
 
 // RouteContext returns mix's routing Context object from a
