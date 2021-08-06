7f0e3ba59bab52c02ccc7ee3085c74ab15b044b8
Alejandro Mery <amery@jpi.io>
2021-08-06 01:02:28 +0100

    context.RoutingContext: add primitive RouteParams methods
    Signed-off-by: Alejandro Mery <amery@jpi.io>

---
+++ context/params.go
@@ -0,0 +1,45 @@
+package context
+
+// Sets value for a RouteParam
+func (rctx *RoutingContext) Set(key string, v interface{}) {
+	if rctx.RouteParams == nil {
+		rctx.RouteParams = make(map[string]interface{}, 1)
+	}
+
+	rctx.RouteParams[key] = v
+}
+
+// Append value for a RouteParam
+func (rctx *RoutingContext) Add(key string, v interface{}) {
+	var vv []interface{}
+
+	if rctx.RouteParams == nil {
+		rctx.RouteParams = make(map[string]interface{}, 1)
+	}
+
+	// extract previous slice if available
+	// and convert single item entries to slices
+	if w, ok := rctx.RouteParams[key]; ok {
+		switch ww := w.(type) {
+		case []interface{}:
+			vv = ww
+		case interface{}:
+			vv = append(vv, w)
+		}
+	}
+
+	// and append our value
+	vv = append(vv, v)
+	rctx.RouteParams[key] = vv
+}
+
+// Get item from RouteParams
+func (rctx *RoutingContext) Get(key string) (interface{}, bool) {
+	if rctx.RouteParams != nil {
+		if w, ok := rctx.RouteParams[key]; ok {
+			return w, ok
+		}
+	}
+
+	return nil, false
+}
